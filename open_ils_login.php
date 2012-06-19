d5743878e66c48ea6f87240e4fd85d4be157a425
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-19 10:47:37 +0530

    object to aray function, open_ils_simple_request

--- open_ils_login.php
+++ open_ils_login.php
@@ -1,6 +1,6 @@
 <?php
-require('token.php');
-
+require('open_ils_simple_request.php');
+require('stdclass_to_array.php');
 function open_ils_login($username, $password)
 {
 $arr = array($username);
@@ -8,14 +8,22 @@ $m = 'open-ils.auth.authenticate.init';
 $s = 'open-ils.auth';
 
 
-$response0 = token($arr, $m, $s);
+$response0 = open_ils_simple_request($arr, $m, $s);
 
 $seed = $response0->result;
 $password = md5($seed . md5($password));
 
 $arr = array("username"=>$username, "password"=>$password, "type"=>"opac"); 
-$response1 = token(array($arr), $m = 'open-ils.auth.authenticate.complete', $s = 'open-ils.auth');
+$response1 = open_ils_simple_request(array($arr), $m = 'open-ils.auth.authenticate.complete', $s = 'open-ils.auth');
+$login_response = stdclass_to_array($response1);
+if ($login_response['result']['ilsevent']=='0')
+{
+return $login_response['result']['payload']['authtoken'];
+}
+else
+return $response1;
 
+/*
 $variables = get_object_vars($response1->result); 
 $keys = array_keys($variables);
 $login_response = array(
@@ -27,6 +35,7 @@ $login_response = array(
 "authtoken"=>$variables[$keys[5]]->authtoken,
 "authtime"=>$variables[$keys[5]]->authtime);
 return $login_response;
+*/
 }
 
 ?>
