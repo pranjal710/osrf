b4bab683a2267d965aad16f5dcfa7c50bd46dbbc
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-22 00:54:19 +0530

    Indenting the code

--- fieldmapper.php
+++ fieldmapper.php
@@ -1,35 +1,26 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set('display_errors', '1');
-
 include ("parse_xml_2_array.php");
+
 $myFile = "classfieldmapper.php";
 $fh = fopen($myFile, 'w') or die("can't open file");
-
 $stringData = "<?php \n \n";
 fwrite($fh, $stringData);
-
 $stringData = "include (\"fieldmapper_class_abstract.php\"); \n ";
 fwrite($fh, $stringData);
-
-for($i=0 ; $class[$i] != NULL ; $i++)
-{
-$id = NULL;
-$stringData = "Class ".$class[$i]." extends fieldmapper_class{\nprivate \$properties = array(";
-$id = $class[$i];
-$data = NULL;
-fwrite($fh, $stringData);
-	for($j=0 ; $field[$id][$j] != NULL ; $j++)
-	{
-	$data = $data.'"'.$field[$id][$j].'", ';
+for($i=0 ; $class[$i] != NULL ; $i++) {
+	$id = NULL;
+	$stringData = "Class ".$class[$i]." extends fieldmapper_class{\nprivate \$properties = array(";
+	$id = $class[$i];
+	$data = NULL;
+	fwrite($fh, $stringData);
+	for($j=0 ; $field[$id][$j] != NULL ; $j++) {
+		$data = $data.'"'.$field[$id][$j].'", ';
 	}
-$stringData = substr($data, 0, -2);
-fwrite($fh, $stringData);
-
-$stringData = "); \n} \n \n";
-fwrite($fh, $stringData);
+	$stringData = substr($data, 0, -2);
+	fwrite($fh, $stringData);
+	$stringData = "); \n} \n \n";
+	fwrite($fh, $stringData);
 }
-
 $stringData = "?>";
 fwrite($fh, $stringData);
 fclose($fh);
--- fieldmapper_class_abstract.php
+++ fieldmapper_class_abstract.php
@@ -1,23 +1,24 @@
 <?php 
+abstract class fieldmapper_class
+	{
+		private $values = array();
+		function __set($field, $value) 
+			{
+				if (in_array($field, $this->$properties)) {
+					$this->values[$field] = $value;
+				}
+				else
+					throw new Exception('fieldmapper class '.get_called_class().' has no '.$field.' . Invalid Field Parameter');
+			}
+		function __get($field) 
+			{
+				if (in_array($field, $this->$values)) {
+					return $this->$values[$field];
+			}
+				else {
+					throw new Exception('fieldmapper class '.get_called_class().' has no '.$field.' . Invalid Field Parameter');
+				}
+			}
 
-abstract class fieldmapper_class{
-private $values = array();
-
-function __set($field, $value) 
-{
-$this->values[$field] = $value;
-}
-
-function __get($field) 
-{
-if (array_key_exists($field, $this->$values)) {
-            return $this->$values[$field];
-        }
-else
-return NULL;
-        
-}
-
-}
-
+	}
 ?>
