663a63b57808d173a75fb9ab4cbd671b317b1a74
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-21 22:19:24 +0530

    Re- Update fieldmapper_class_abstract.php

--- fieldmapper_class_abstract.php
+++ fieldmapper_class_abstract.php
@@ -13,8 +13,14 @@ function __get($field)
 if (array_key_exists($field, $this->$values)) {
             return $this->$values[$field];
         }
-else
-return NULL;
+        
+$trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): ' . $field .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
         
 }
 
