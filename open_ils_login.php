e924d0d753c7f0eaf16b093b0dd76619629a6f9c
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-23 22:28:26 +0530

    indented

--- open_ils_login.php
+++ open_ils_login.php
@@ -2,26 +2,24 @@
 require('open_ils_simple_request.php');
 require('stdclass_to_array.php');
 function open_ils_login($username, $password)
-{
-$arr = array($username);
-$m = 'open-ils.auth.authenticate.init';
-$s = 'open-ils.auth';
+     {
+        $arr = array($username);
+        $m = 'open-ils.auth.authenticate.init';
+        $s = 'open-ils.auth';
 
+        $response0 = open_ils_simple_request($arr, $m, $s);
 
-$response0 = open_ils_simple_request($arr, $m, $s);
+        $seed = $response0->result;
+        $password = md5($seed . md5($password));
 
-$seed = $response0->result;
-$password = md5($seed . md5($password));
-
-$arr = array("username"=>$username, "password"=>$password, "type"=>"opac"); 
-$response1 = open_ils_simple_request(array($arr), $m = 'open-ils.auth.authenticate.complete', $s = 'open-ils.auth');
-$login_response = stdclass_to_array($response1);
-if ($login_response['result']['ilsevent']=='0')
-{
-return $login_response['result']['payload']['authtoken'];
-}
-else
-return $login_response['result'];
+        $arr = array("username"=>$username, "password"=>$password, "type"=>"opac"); 
+        $response1 = open_ils_simple_request(array($arr), $m = 'open-ils.auth.authenticate.complete', $s = 'open-ils.auth');
+        $login_response = stdclass_to_array($response1);
+        if ($login_response['result']['ilsevent']=='0') {
+              return $login_response['result']['payload']['authtoken'];
+        }
+        else
+              return $login_response['result'];
 /*
 $variables = get_object_vars($response1->result); 
 $keys = array_keys($variables);
@@ -36,5 +34,4 @@ $login_response = array(
 return $login_response;
 */
 }
-
 ?>
