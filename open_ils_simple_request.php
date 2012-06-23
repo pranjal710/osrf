0013cdd52b46a0c3958c39a51ddf0a466b9f57b3
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-23 22:26:21 +0530

    indented

--- open_ils_simple_request.php
+++ open_ils_simple_request.php
@@ -4,26 +4,23 @@ require('parse.php');
 require('osrfResponse.php');
 
 function open_ils_simple_request($arr, $m, $s)
-{
-$endpoint = 'localhost'; //input
-if ($endpoint)
-{
-$a = new osrfMessage($m, $s, $arr, $endpoint);
-}
-else
-$a = new osrfMessage($m, $s, $arr);
+	{
+		$endpoint = 'localhost'; //input
+		if ($endpoint) {
+			$a = new osrfMessage($m, $s, $arr, $endpoint);
+		}
+		else
+			$a = new osrfMessage($m, $s, $arr);
 
 	{
-	$response = $a->send();
-	if ($response) 
-	{
-	$response->parse();
-	}
+		$response = $a->send();
+		if ($response) {
+			$response->parse();
+		}
 	
-	else echo "Unable to reach the service";
+		else echo "Unable to reach the service";
 	}
 
-return $response;
+		return $response;
 }
-//
 ?>
