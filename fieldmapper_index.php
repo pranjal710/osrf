ead94e77816b68fbd17995d9d8ea69fe401b5c48
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-06-23 22:49:16 +0530

    added decodejson2object

--- fieldmapper_index.php
+++ fieldmapper_index.php
@@ -1,30 +1,12 @@
 <?php
 include ("fieldmapper.php");
 include ("classfieldmapper.php");
-include ("stdclass_to_array.php");
-
+include ("decodejson2obj.php");
 $obj = new ancc();
-$obj->item_type = 'T';
-echo "<strong>Fieldmapper_index: </strong><br /><pre>";
-print_r ($obj);
-echo "</pre><br />";
-echo json_encode($obj->encodeForOpenSRF())."<HR />";
-$json_obj = json_encode($obj->encodeForOpenSRF());
-
-$json_obj_parsed = stdclass_to_array(json_decode($json_obj));
-echo "Class: <pre>"; print_r ($json_obj_parsed['__c']); echo "<pre>";
-echo "Parameters: <pre>"; print_r ($json_obj_parsed['__p']); echo "<pre>";
-
-$class = $json_obj_parsed['__c'];
-$parameter = $json_obj_parsed['__p'];
-$new = new $class();
-for ($c = 0 ; $c < (count($parameter)) ; $c++) {
-	$key1 = $new->_properties[$c];
-	if (isset($parameter[$c])) {
-		$new->$key1 = $parameter[$c];
-	}
-}
-echo "<strong>New Object: </strong><br /><pre>";
-print_r ($new);
-echo "</pre><br />"
+$obj->circ_time = 'now';
+$obj->circ_lib = new aou();
+//$obj->circ_lib->id = 1;
+echo "<pre>"; print_r ($obj); echo "</pre>";
+echo json_encode($obj->encodeForOpenSRF());
+//decodejson2obj(json_encode($obj->encodeForOpenSRF()))
 ?>
