6acd2d9a364450cc93f102e4f24b3d86a127c014
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-07-02 19:55:19 +0530

    decodejson2object

--- decodejson2obj.php
+++ decodejson2obj.php
@@ -3,21 +3,36 @@ include ("stdclass_to_array.php");
 
 function decodejson2obj($json_obj) {
 	$json_obj_parsed = stdclass_to_array(json_decode($json_obj));
-	//echo "Class: <pre>"; print_r ($json_obj_parsed['__c']); echo "<pre>";
-	//echo "Parameters: <pre>"; print_r ($json_obj_parsed['__p']); echo "<pre>";
-
 	$class = $json_obj_parsed['__c'];
 	$parameter = $json_obj_parsed['__p'];
 	$new = new $class();
 	for ($c = 0 ; $c < (count($parameter)) ; $c++) {
 		$key1 = $new->_properties[$c];
 		if (isset($parameter[$c])) {
-		$new->$key1 = $parameter[$c];
+		if(is_array($parameter[$c])) {
+			$x = $parameter[$c];
+			$new->$key1 = recursive($x);
+		}
+		else $new->$key1 = $parameter[$c];
 		}
 	}
-	echo "<strong>New Object: </strong><br /><pre>";
-	print_r ($new);
-	echo "</pre><br />";
+	return $new;
+}
+
+function recursive($obj) {
+	$class = $obj['__c'];
+	$parameter = $obj['__p'];
+	$new = new $class();
+	for ($c = 0 ; $c < (count($parameter)) ; $c++) {
+		$key1 = $new->_properties[$c];
+		if (isset($parameter[$c])) {
+		if(is_array($parameter[$c])) {
+			$x = $parameter[$c];
+			$new->$key1 = recursive($x);
+		}
+		else $new->$key1 = $parameter[$c];
+		}
+	}
 	return $new;
 }
 ?>
