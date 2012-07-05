dd9ab2bf63d6aee4f01bf544dc77729e323a7af8
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-07-06 02:28:02 +0530

    commit

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 include ("open_ils_login.php");
-$t = open_ils_login('username', 'password');
-echo "<pre>";
-print_r ($t);
-echo "</pre>";
+include ("fieldmapper.php");
+include ("classfieldmapper.php");
+include ("decodejson2obj.php");
+include ("OpenSRFSession.php");
+
+$ses = new OpenSRFSession("117.200.83.65"); // remembers server & loads
+                                                 // fieldmapper.
+$authtoken = $ses->login('pranjal', 'prabhasH1');      ////  $t = Authentication token
+//echo $auth;
+
+$hold = new ahr();
+$hold->target = 3;
+$hold->hold_type = "T";
+$hold->pickup_lib = 4;
+$hold->request_lib = 4;
+$hold->requestor = 1;
+$hold->usr = 1;
+
+echo "<HR />";
+$response = $ses->request("open-ils.circ", "open-ils.circ.holds.create", $authtoken, $hold);
+echo "<pre>"; print_r ($response); echo "</pre>";
+/*if (is_openils_event($response)) {
+   echo "Could not place hold because of error: " . $response["desc"];
+} else {
+   echo "Placed hold successfully. ID is " . $response;
+}
+*/
 ?>
