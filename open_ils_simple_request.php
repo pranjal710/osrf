5726e721c08ec00d3c6a4e3137a383d3ff2b5562
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-07-09 00:14:45 +0530

    removed globals

--- open_ils_simple_request.php
+++ open_ils_simple_request.php
@@ -3,9 +3,8 @@ require('osrfMessage.php');
 require('parse.php');
 require('osrfResponse.php');
 
-function open_ils_simple_request($arr, $m, $s) {
-	global $ses;
-	$endpoint = $ses->server;
+function open_ils_simple_request($arr, $m, $s, $server) {
+	$endpoint = $server;
 	if ($endpoint) {
 	$a = new osrfMessage($m, $s, $arr, $endpoint);
 	}
