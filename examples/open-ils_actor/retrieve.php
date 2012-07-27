bd0a8618d2ad1570ea8c91358f6bcb545a8c61b1
Pranjal Prabhash <pranjal.prabhash@gmail.com>
2012-07-27 20:02:58 +0530

    retrieve.php

--- examples/open-ils_actor/retrieve.php
+++ examples/open-ils_actor/retrieve.php
@@ -1,26 +1,36 @@
 <?php
-function tree($a, $n = 0)
+function tree($a, &$obj)
 {
     $f = 1;
     if (array_key_exists('__p', $a)) {
-	$n = $n + 2;
-	tree($a['__p'], $n);
+	//if($field != null) $obj->$field = new aou;
+	tree($a['__p'], $obj);
     $f = 2;
     }
 
-    else if (is_array($a[0]) && (!(empty($a[0])))) {
-    $n = $n + 2;
-	for ( $j = 0 ; is_array($a[$j]) ; $j++) {
-	    tree($a[$j], $n);
-	}
-    $f = 2;
-    }
+    else {
+    for ( $j = 0 ; $j < (count($a)) ; $j++) {
+		if (is_array($a[$j]) && (!(empty($a[$j])))) {
+			if (array_key_exists('__p', $a[$j])) {
+				$k = new aou(); $field = $k->_properties[$j];
+				$obj->$field = new aou();
+				tree($a[$j], $obj->$field);
+				$f = 2;
+			}
+			else {
+				for ( $k = 0 ; $k < (count($a[$j])) ; $k++)
+				  {
+					if (is_array($a[$j][$k]) && (!(empty($a[$j][$k]))))
+					tree($a[$j][$k], $obj);
+				  }
+			}
+		}
+    
+	}    
+   }
     
     if ($f == 1) {
-	    for ( $i = 0 ; $i < $n; $i++ ) {
-		    echo "&nbsp;&nbsp;";
-		}
-		echo $a[6]."<br />";
+		$obj->name = $a[6];
 	}
 }
 ?>
@@ -28,7 +38,7 @@ function tree($a, $n = 0)
 <?php
 include ("./../../osrfSession.php");
 
-$ses = new osrfSession("hostname"); // e.g.: localhost  remembers server & loads fieldmapper.
+$ses = new osrfSession("localhost"); // e.g.: localhost  remembers server & loads fieldmapper.
 if ($ses->checkhost() == 200) {
 	
 	try {
@@ -47,9 +57,14 @@ if ($ses->checkhost() == 200) {
 		if (Is_Open_Ils_event($response)) {
 			echo "Could not place hold because of error: " . $response['result']["desc"];
 		} 
-		else tree($response['result']);
+		else {
+			$obj = new aou();
+			tree($response['result'], $obj); 
+echo "<pre>"; print_r ($obj); echo "</pre>";
+		}
 	}
 	else echo "Errors were encountered.";
 }
 else echo "Could not locate fieldmapper. Invalid hostname!";
 ?>
+
